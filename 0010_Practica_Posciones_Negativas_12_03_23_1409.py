205cb882ab22732e9a24e5abd3d942eb6d5405ee
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 14:09:14 -0600

    Create 0010_Practica_Posciones_Negativas_12_03_23_1409.py

---
+++ 0010_Practica_Posciones_Negativas_12_03_23_1409.py
@@ -0,0 +1,10 @@
+#Ejercicio 
+#Utiliza las posiciones negativas para acceder e imprimir algunos de los colores de esta lista. Los colores a los que tienes que acceder son 'naranja', 'amarillo', 'lila', 'blanco' y 'rojo'.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila','negro', 'rosa', 'blanco', 'naranja']
+
+print(colores[-1]) #Los negativos empiezan en -1 con el ultimo
+print(colores[-7])
+print(colores[-5])
+print(colores[-2])
+print(colores[-10]) #El primero que seria 0 en negativo seria el numero negativo del numero de elementos
