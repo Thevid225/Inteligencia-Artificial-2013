d46e0b6c12254c320630f48b9fadbb1ab7c8004e
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 16:10:53 -0600

    Create 0019_Practica_Condicional_If_12_03_23_1610.py

---
+++ 0019_Practica_Condicional_If_12_03_23_1610.py
@@ -0,0 +1,24 @@
+#Ejercicio
+#1.-Cambia el operador para que la condición sea True
+
+num1 = 15
+num2 = 20
+
+if num1 < num2: #Cambiar el == por el < ya que num1 es menor que num2
+	print('Se ejecuta el if.')
+  
+#2.-Cambia el operador para que la condición sea True.
+ 
+num3 = 1450
+num4 = 60
+
+if num3 > num4: #Se pone > ya que num3 es mayor que num4
+	print('Se ejecuta el if.')
+  
+#Haz que el siguiente condicional se convierta en False sin cambiar el operador.
+
+num5 = 1450
+num6 = 60
+
+if num5 == num6: #Ningun numero es igual asi que es falso
+	print('Se ejecuta el if.')
