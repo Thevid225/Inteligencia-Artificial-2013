9f7719ebd03a3bf3f36c71ff10b2c83fc5b9714e
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 13:05:16 -0600

    Create 0001_Practica_Variables_12_03_23_1304.py

---
+++ 0001_Practica_Variables_12_03_23_1304.py
@@ -0,0 +1,13 @@
+msj = "Hola mundo" #Se declara la variable de tipo string
+print (msj) #Se imprime los que hay en la variable
+
+msj = 18 #Cambiamos lo que hay en la variable y tambien el tipo de variable si quisieramos seguir con tipo string pondriamos '10'
+print (msj + 11) #Se realiza la suma de lo que hay dentro del () no se podria si lo que hay en la variale fuera string
+
+#Ejercicio
+
+num1, num2 = 18,11 #Declaramos las dos variables con sus valores respectivos
+print (num1,num2) # los imprimimos para ver que esta bien
+
+res = num1 +num2 #Declaramos donde llega la suma y aplicamos la suma
+print (res) # imprimimos el resultado
