798e80699837a41d96abaf4af1fa44817f3e436d
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 15:36:05 -0600

    Create 0013_Practica_Agregar_Lista_12_03_23_1535.py

---
+++ 0013_Practica_Agregar_Lista_12_03_23_1535.py
@@ -0,0 +1,8 @@
+#Ejercicio
+#Añade los colores 'fuxia' y 'celeste' a la lista.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+colores.append('fuxia') #Usamos la funcion append para agregar al ultimo
+colores.append('celeste') 
+
+print(colores)
