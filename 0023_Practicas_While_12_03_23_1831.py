e0a7f774c454be10f4562446779f680924405071
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 17:31:50 -0600

    Create 0023_Practicas_While_12_03_23_1831.py

---
+++ 0023_Practicas_While_12_03_23_1831.py
@@ -0,0 +1,21 @@
+#Ejercicios
+#1.-Crea un bucle while que se ejecute hasta que x valga 15 con incrementos de 5.
+
+x=0
+while x <= 15:
+    print (x)
+    x=x+5
+
+#2.-Crea un bucle while que se ejecute hasta que x valga -100 con decrementos de 20.
+  
+x1 = 0
+while x1 >= -100:
+    print (x1)
+    x1 -= 20
+
+#3.-Crea un bucle while que se ejecute hasta que x valga 0 con decrementos de 1 y que muestre en cada ejecución esta frase con el valor de ejecución correspondiente: 'El valor del bucle es 10'.
+
+x2 = 10
+while x2 >= 0:
+    print ('El valor del bucle es: ',x2)
+    x2 -= 1
