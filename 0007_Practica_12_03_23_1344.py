bc6f0fe15a3ba374d9b09c7e66f8788ccbf82c14
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 13:44:30 -0600

    Create 0007_Practica_12_03_23_1344.py

---
+++ 0007_Practica_12_03_23_1344.py
@@ -0,0 +1,5 @@
+#Ejercicio Utilizando solo el número 2 (cuantas veces necesites) y sin utilizar exponentes, debes obtener el resultado 1024.
+
+milv = 2*2*2*2*2*2*2*2*2*2
+milvc = 2**10 
+print (milvc) #comprobamos
