28551871147bb069d3a61d70cc953e178481caea
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 15:58:01 -0600

    Create 0017_Practica_Tuplas_12_03_23_1557.py

---
+++ 0017_Practica_Tuplas_12_03_23_1557.py
@@ -0,0 +1,10 @@
+#Ejercicio
+#Imprime la segunda posición de esta tupla.
+
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+print (colores[2])
+
+#Utiliza los símbolos de suma y resta para obtener el resultado 25 a partir de los elementos de la siguiente tupla en una variable llamada operacion.
+
+num = (10, 1, 5, 11)
+print(num[0] - num[1] + num[2] + num[3])
