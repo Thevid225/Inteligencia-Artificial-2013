f66f4517f88ddb6bfd8e5ac18de310c240be9935
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 15:46:07 -0600

    Create 0015_Practica_Ordenar_Lista_12_03_23_1545.py

---
+++ 0015_Practica_Ordenar_Lista_12_03_23_1545.py
@@ -0,0 +1,5 @@
+#Ejercicio
+#Ordena la siguiente lista en orden alfabético descendente (de la letra 'z' a la 'a').
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón','lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(sorted(colores)) #sorted los imprime directamente en orden alfabetico
