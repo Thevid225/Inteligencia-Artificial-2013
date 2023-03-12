dba31b0c290724c9d34f6befc898f971c523476e
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 14:24:38 -0600

    Create 0012_Practica_Eliminar_pop_12_03_23_1424.py

---
+++ 0012_Practica_Eliminar_pop_12_03_23_1424.py
@@ -0,0 +1,10 @@
+#Ejercicio
+#Elimina de la siguiente lista los elementos 'azul' y 'blanco'. Solo puedes eliminarlos utilizando el método pop(). Además, tendrás que guardar esos dos colores en una nueva lista.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+col1 = colores.pop(1)
+col2 = colores.pop(7) #Tambien se mueve la lista
+
+colel=[col1,col2] #Creamos la nueva lista
+print(colel) #imprimimos la nueva lista
