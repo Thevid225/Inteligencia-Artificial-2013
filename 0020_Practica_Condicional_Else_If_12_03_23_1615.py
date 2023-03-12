594721ff3d3fe11775f2282da8d2219b26a27a3a
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 16:16:06 -0600

    Create 0020_Practica_Condicional_Else_If_12_03_23_1615.py

---
+++ 0020_Practica_Condicional_Else_If_12_03_23_1615.py
@@ -0,0 +1,10 @@
+#Ejercicio
+#Corrige el siguiente condicional if else
+
+color = rojo
+#Estaba alreves el if y el else
+if color != rojo:
+	print ("El color no es rojo.")  #Para que funcione el if se necesita un espacio en las lineas que va a afectar
+#En el print faltaban los parentesis
+else color == rojo:  
+	print ("El color es rojo.")
