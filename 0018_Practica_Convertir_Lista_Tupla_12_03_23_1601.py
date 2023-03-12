c0afc1759e26591adada89593e71989dda93d690
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 16:01:58 -0600

    Create 0018_Practica_Convertir_Lista_Tupla_12_03_23_1601.py

---
+++ 0018_Practica_Convertir_Lista_Tupla_12_03_23_1601.py
@@ -0,0 +1,6 @@
+#Ejercicio
+#Convierte la siguiente lista en una tupla y asegúrate que se haya convertido en tupla correctamente imprimiendo en la consola el tipo de elemento que es.
+
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+lista = list(colores) #Aqui se cambia de tipo tuplaa tipo lista 
+print (type(lista)) #Se imprime el tipo que es lista si todo bien debe decir 'list'
