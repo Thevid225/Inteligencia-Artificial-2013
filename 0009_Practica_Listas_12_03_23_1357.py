5c3317c3890a0697bd061e7c456c3b0e4903cac3
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 13:58:06 -0600

    Create 0009_Practica_Listas_12_03_23_1357.py

---
+++ 0009_Practica_Listas_12_03_23_1357.py
@@ -0,0 +1,18 @@
+#Ejercicio 
+#1.-De la siguiente lista, ¿qué color está en la posición 3?
+#colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+#2.-¿En qué posición se encuentra el color 'rojo'? ¿y el 'rosa'?
+
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"] #Decalramos la lista
+print (colores[3])#Imprimimos el que esta en el espacio 3
+print (colores[0],colores[7]) #Imprimimos rojo y rosa
+
+#Crea una lista que contenga los siguientes valores en las posiciones indicadas.
+#"uno" en la posición 4.
+#"dos" en la posición 1.
+#"tres" en la posición 0.
+#"cuatro" en la posición 3.
+#"cinco" en la posición 2.
+
+numeros = ["tres","dos","cinco","cuatro","uno"] #Creamos la lista
+print (numeros)
