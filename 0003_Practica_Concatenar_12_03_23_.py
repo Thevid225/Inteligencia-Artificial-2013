99c562821858de0304c532b24177406ad3070171
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 13:15:44 -0600

    Create 0003_Practica_Concatenar_12_03_23_.py

---
+++ 0003_Practica_Concatenar_12_03_23_.py
@@ -0,0 +1,16 @@
+#Ejercicio
+
+wrd1 = 'Hola'
+wrd2 = 'Mundo'
+fras = wrd1 + wrd2 #Concatenamos primero con otra variable 
+print (fras,"\n") #Imprimimos la concatenacion
+print (wrd1 + wrd2) #Concatenamos en el print
+
+nom = 'David'
+ape = 'Patiño'
+nomc = nom + ape 
+print (nomc,"\n")
+
+num1 = '18'
+num2 = '11'
+print (num1 + num2)
