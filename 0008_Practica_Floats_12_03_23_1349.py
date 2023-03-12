c4412aac9eb31f1de75b1a699a403452bfd77175
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 13:49:31 -0600

    Create 0008_Practica_Floats_12_03_23_1349.py

---
+++ 0008_Practica_Floats_12_03_23_1349.py
@@ -0,0 +1,4 @@
+#Ejercicio redondea con cinco decimales el número 17.567383292929200234.
+
+num = 17.567383292929200234 #Declaramos el float con el numero requerido
+print(round(num,5)) #Usamos round() para redondear
