47debc27dec070d5b136c77a85f68fff77d47b78
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 13:40:38 -0600

    Create 0006_Pratica_Operaciones1_12_03_23_1340.py

---
+++ 0006_Pratica_Operaciones1_12_03_23_1340.py
@@ -0,0 +1,9 @@
+#Ejercicio
+
+suma = 20+23+30+14 #Sumamos para que de 87
+rest = 20+23-50-30-40-10 #Restamos para que de -87
+mult = 20*23*1.8913 #Mutiplicamos para que de 870
+divi = 5000/230/2 #Dividimos para que de 10
+oper = 10/5+15-17 #El resultado debe ser 0 con los numeros 10_5_15_17 usando las cuatro operaciones
+
+print ("Suma: ",suma,"\nResta: ",rest,"\nMultiplicacion: ",mult,"\nDivision: ",divi,"\nOperacion: ",oper) 
