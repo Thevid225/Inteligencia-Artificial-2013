0e2f119abda76b836e13d8cbb37ac649a2cb4f77
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 20:09:00 -0600

    Create 0027_Practica_Diccionarios_12_03_23_2008.py

---
+++ 0027_Practica_Diccionarios_12_03_23_2008.py
@@ -0,0 +1,16 @@
+#Ejercicio
+#Del diccionario teclado2 del capítulo, muestra los elementos Modelo y Precio con presentación en un print(). El resultado será esto en la consola:
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+print('Modelo ',teclado2['Modelo'],'precio ',teclado2['Precio'])
