623003efe2a5458c9483c9539620122e29b2a917
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 20:12:13 -0600

    Create 0028_Practica_Diccionarios_For_12_03_23_2011.py

---
+++ 0028_Practica_Diccionarios_For_12_03_23_2011.py
@@ -0,0 +1,17 @@
+#Ejercicio
+#Itera el diccionario teclado1 con un solo bucle for que muestre esto en la consola:
+
+teclado1 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'HyperX Alloy FPS Pro',
+	'Precio': '89,99'
+}
+
+teclado2 = {
+	'Categoría': 'Teclados',
+	'Modelo': 'Corsair K55 RGB',
+	'Precio': '59,99'
+}
+
+for x in teclado2:
+	print(x, '=',teclado2[x])
