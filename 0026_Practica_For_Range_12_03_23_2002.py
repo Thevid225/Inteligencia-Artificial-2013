792ad3ce9ddfb26008400726e0dc4401dd1b6101
David Patiño Arvizu <127183279+Thevid225@users.noreply.github.com>
2023-03-12 20:02:50 -0600

    Create 0026_Practica_For_Range_12_03_23_2002.py

---
+++ 0026_Practica_For_Range_12_03_23_2002.py
@@ -0,0 +1,5 @@
+#Ejercicio
+#Crea un bucle for con un range() que vaya desde el valor 7 hasta el valor 700 en saltos de 100. Basta con que imprimas el valor de cada iteración.
+
+for x in range(7, 700, 100): #Se le marcan de donde empieza hasta donde termina
+    print(x)
